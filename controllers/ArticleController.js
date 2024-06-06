681f40cae60bb5b445b857984c081ff519d34185
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-06 18:11:04 +0200

    refacto(merged)

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -4,30 +4,40 @@ const mongoose = require("mongoose");
 module.exports = router;
 const Article = require("../models/article");
 
-router.get("/restaurants/:restaurantId/articles", async (req, res) => {
-  const { restaurantId } = req.params;
+router.get("/:restaurantId", async (req, res) => {
   try {
-    const articles = await Article.find({ restaurant_id: restaurantId });
+    const articles = await Article.find({
+      restaurant_id: req.params.restaurantId,
+    });
+    if (!articles) {
+      return res
+        .status(404)
+        .json({ message: "Articles not found for this restaurant!" });
+    }
     res.json(articles);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-router.delete("/articles/delete/:articleId", async (req, res) => {
-  const { articleId } = req.params;
+router.delete("/:articleId", async (req, res) => {
   try {
-    const objectId = new mongoose.Types.ObjectId(articleId);
-    const article = await Article.findByIdAndDelete(objectId);
-    res.json({ message: "Article deleted successfully" });
+    const article = await Article.findByIdAndDelete(req.params.articleId);
+    if (!article) {
+      return res.status(404).json({ message: "Article not found!" });
+    }
+    res.json({ message: "Article deleted successfully!" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-router.post("/articles", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const article = new Article(req.body);
+    if (!article) {
+      return res.status(404).json({ message: "Error while adding article!" });
+    }
     await article.save();
     res.status(201).send(article);
   } catch (error) {
@@ -35,13 +45,19 @@ router.post("/articles", async (req, res) => {
   }
 });
 
-router.put("/articles/:id", async (req, res) => {
+router.put("/:articleId", async (req, res) => {
   try {
-    const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    await article.save();
+    const article = await Article.findByIdAndUpdate(
+      req.params.articleId,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!article) {
+      return res.status(404).json({ message: "Article not found!" });
+    }
     res.json(article);
   } catch (error) {
     res.status(400).send(error);
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const menusRouter = require("./controllers/MenuController");
-const notificationsRouter = require("./controllers/NotificationController");
-const ordersRouter = require("./controllers/OrderController");
 const articlesRouter = require("./controllers/ArticleController");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 3006;
 
 app.use(cors());
 
@@ -25,9 +22,6 @@ db.mongoose
 
 app.use(bodyParser.json());
 
-app.use("/api", menusRouter);
-app.use("/api", notificationsRouter);
-app.use("/api", ordersRouter);
-app.use("/api", articlesRouter);
+app.use("/", articlesRouter);
 
-app.listen(port, () => console.log("app running on http://localhost:3000"));
+app.listen(port, () => console.log(`app running on http://localhost:${port}`));
