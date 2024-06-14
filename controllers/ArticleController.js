3649f8f9de3a42e71b2eca3d4dbf3e1ce5fc73e8
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-14 14:23:05 +0200

    feature(add pagination + res.status)

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,20 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 module.exports = router;
 const Article = require("../models/article");
 
 router.get("/:restaurantId", async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
     const articles = await Article.find({
       restaurant_id: req.params.restaurantId,
+    })
+      .skip(skip)
+      .limit(limit);
+
+    const totalArticles = await Article.countDocuments({
+      restaurant_id: req.params.restaurantId,
     });
+
     if (!articles) {
-      return res
-        .status(404)
-        .json({ message: "Articles not found for this restaurant!" });
+      return res.status(404).json({ message: "Not found" });
     }
-    res.json(articles);
+
+    if (articles.length === 0) {
+      return res.status(201).json({ message: "Empty" });
+    }
+
+    res.status(201).json({
+      totalPages: Math.ceil(totalArticles / limit),
+      articles: articles,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -23,10 +39,12 @@ router.get("/:restaurantId", async (req, res) => {
 router.delete("/:articleId", async (req, res) => {
   try {
     const article = await Article.findByIdAndDelete(req.params.articleId);
+
     if (!article) {
-      return res.status(404).json({ message: "Article not found!" });
+      return res.status(404).json({ message: "Not found" });
     }
-    res.json({ message: "Article deleted successfully!" });
+
+    res.status(201).json({ message: "Item deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -35,13 +53,16 @@ router.delete("/:articleId", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const article = new Article(req.body);
+
     if (!article) {
-      return res.status(404).json({ message: "Error while adding article!" });
+      return res.status(404).json({ message: "Not found" });
     }
+
     await article.save();
-    res.status(201).send(article);
+
+    res.status(201).json({ message: "Item posted" });
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).json({ message: error.message });
   }
 });
 
@@ -55,10 +76,12 @@ router.put("/:articleId", async (req, res) => {
         runValidators: true,
       }
     );
+
     if (!article) {
-      return res.status(404).json({ message: "Article not found!" });
+      return res.status(404).json({ message: "Not found!" });
     }
-    res.json(article);
+
+    res.status(201).json({ message: "Item updated" });
   } catch (error) {
     res.status(400).send(error);
   }
